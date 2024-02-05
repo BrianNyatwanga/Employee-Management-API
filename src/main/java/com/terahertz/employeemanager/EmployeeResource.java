7d30a8db4f8daa64c68498d1a48eabfd4260e70d
Brian Nyatwanga <briannyatwanga@gmail.com>
2024-02-05 16:43:07 +0300

    Added cross origin

--- src/main/java/com/terahertz/employeemanager/EmployeeResource.java
+++ src/main/java/com/terahertz/employeemanager/EmployeeResource.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/employee")
+@RequestMapping("/employee")
+@CrossOrigin
 public class EmployeeResource {
     private final EmployeeService employeeService;
 
